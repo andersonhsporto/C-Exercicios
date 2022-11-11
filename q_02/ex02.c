a3bad20665f21e06cb257c3cdefc8a790eb239c7
andersonhsporto <anderson.higo2@gmail.com>
2022-11-11 19:20:22 -0300

    fix: code style changes

--- q_02/ex02.c
+++ q_02/ex02.c
@@ -4,75 +4,70 @@
 #include <string.h>
 
 char *reader();
+
 bool isRoman(char *str);
+
 int romanToInt(char c);
-size_t	romanConverter(char *string);
+
+size_t romanConverter(char *string);
 
 int main() {
-	char	*line = reader();
-	size_t	romanNumber = romanConverter(line);
+  char *line = reader();
+  size_t romanNumber = romanConverter(line);
 
-	printf("%zu\n", romanNumber);
-	free(line);
-	return 0;
+  printf("%zu\n", romanNumber);
+  free(line);
+  return 0;
 }
 
 char *reader() {
-	char *line = malloc(10000);
+  char *line = malloc(10000);
 
-	printf("Digite um numero romano: ");
-	fgets(line, 10000, stdin);
-	if (isRoman(line)) {
-		return line;
-	} else  {
-		free(line);
-		printf("Numero invalido, tente novamente.\n");
-		return reader();
-	}
+  printf("Digite um numero romano: ");
+  fgets(line, 10000, stdin);
+  if (isRoman(line)) {
+    return line;
+  } else {
+    free(line);
+    printf("Numero invalido, tente novamente.\n");
+    return reader();
+  }
 }
 
 bool isRoman(char *str) {
-	char *roman = "IVXLCDM";
+  char *roman = "IVXLCDM";
 
-	for (int i = 0; i < strlen(str); i++) {
-		if (strchr(roman, str[i])) {
-			return true;
-		}
-	}
-	return false;
+  for (int i = 0; i < strlen(str); i++) {
+    if (strchr(roman, str[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 int romanToInt(char c) {
-	switch (c) {
-		case 'I':
-			return 1;
-		case 'V':
-			return 5;
-		case 'X':
-			return 10;
-		case 'L':
-			return 50;
-		case 'C':
-			return 100;
-		case 'D':
-			return 500;
-		case 'M':
-			return 1000;
-		default:
-			return 0;
-	}
+  switch (c) {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return 0;
+  }
 }
 
-size_t	romanConverter(char *string) {
-	size_t number = 0;
+size_t romanConverter(char *string) {
+  size_t number = 0;
 
-	for (size_t i = 0; i < strlen(string); i++) {
-		if (romanToInt(string[i]) < romanToInt(string[i + 1])) {
-			number += romanToInt(string[i + 1]) - romanToInt(string[i]);
-			i++;
-		} else {
-			number += romanToInt(string[i]);
-		}
-	}
-	return number;
+  for (size_t i = 0; i < strlen(string); i++) {
+    if (romanToInt(string[i]) < romanToInt(string[i + 1])) {
+      number += romanToInt(string[i + 1]) - romanToInt(string[i]);
+      i++;
+    } else {
+      number += romanToInt(string[i]);
+    }
+  }
+  return number;
 }
